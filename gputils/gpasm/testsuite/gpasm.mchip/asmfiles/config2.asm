6a380f409d3a4cc64f9d8fd3429c1eddb6d410cc
nobody <nobody@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2002-05-12 22:02:01 +0000

    This commit was manufactured by cvs2svn to create tag 'gputils-extra-0_0_2'.

--- gputils/gpasm/testsuite/gpasm.mchip/asmfiles/config2.asm
+++
@@ -1,9 +0,0 @@
-; CONFIG2.ASM
-
-; This file tests the __CONFIG directive for 14-bit core.
-
-	LIST	P=16C61, R=HEX
-
-	__CONFIG	1234
-
-	END
--- gputils/gpasm/testsuite/gpasm.project/asmfiles/baddefine.asm
+++
@@ -1,9 +0,0 @@
-; tests for bug #423850 Hang on bad #define directive
-
-     list p=16c84
-
-#define foo foo
-
-     movlw foo
-
-     end
