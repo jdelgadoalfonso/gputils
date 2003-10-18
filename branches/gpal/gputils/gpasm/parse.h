4d52688f22f5a1c9627cb52fb972dbb8bf424d5b
(no author) <(no author)@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2003-10-18 00:58:37 +0000

    This commit was manufactured by cvs2svn to create branch 'gpal'.
    git-svn-id: svn://svn.code.sf.net/p/gputils/code@189 2421ac41-8990-4fbb-a74b-d7adc9fc73e3

---
+++ branches/gpal/gputils/gpasm/parse.h
@@ -0,0 +1,57 @@
+#ifndef BISON_Y_TAB_H
+# define BISON_Y_TAB_H
+
+#ifndef YYSTYPE
+typedef union {
+  gpasmVal i;
+  char *s;
+  struct pnode *p;
+} yystype;
+# define YYSTYPE yystype
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+# define	LABEL	257
+# define	IDENTIFIER	258
+# define	CBLOCK	259
+# define	ENDC	260
+# define	FILL	261
+# define	NUMBER	262
+# define	STRING	263
+# define	INCLUDE	264
+# define	UPPER	265
+# define	HIGH	266
+# define	LOW	267
+# define	LSH	268
+# define	RSH	269
+# define	GREATER_EQUAL	270
+# define	LESS_EQUAL	271
+# define	EQUAL	272
+# define	NOT_EQUAL	273
+# define	LOGICAL_AND	274
+# define	LOGICAL_OR	275
+# define	ASSIGN_PLUS	276
+# define	ASSIGN_MINUS	277
+# define	ASSIGN_MULTIPLY	278
+# define	ASSIGN_DIVIDE	279
+# define	ASSIGN_MODULUS	280
+# define	ASSIGN_LSH	281
+# define	ASSIGN_RSH	282
+# define	ASSIGN_AND	283
+# define	ASSIGN_OR	284
+# define	ASSIGN_XOR	285
+# define	INCREMENT	286
+# define	DECREMENT	287
+# define	TBL_NO_CHANGE	288
+# define	TBL_POST_INC	289
+# define	TBL_POST_DEC	290
+# define	TBL_PRE_INC	291
+# define	CONCAT	292
+# define	VAR	293
+# define	VARLAB_BEGIN	294
+# define	VAR_BEGIN	295
+# define	VAR_END	296
+
+
+extern YYSTYPE yylval;
+
+#endif /* not BISON_Y_TAB_H */
