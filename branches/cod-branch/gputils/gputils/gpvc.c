93d0fc7fcb276acde4631d843429a0f5d17d673e
craigfranklin <craigfranklin@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2004-09-25 18:27:16 +0000

    changed to byte addressing in 18xx cod files

--- branches/cod-branch/gputils/gputils/gpvc.c
+++ branches/cod-branch/gputils/gputils/gpvc.c
@@ -33,8 +33,6 @@ char *source_file_names[MAX_SOURCE_FILES];
 FILE *source_files[MAX_SOURCE_FILES];
 DirBlockInfo main_dir;
 
-int addrsize;
-
 char directory_block_data[COD_BLOCK_SIZE];
 char * SymbolType4[154] = {
   "a_reg          ", "x_reg          ", "c_short        ", "c_long         ",
@@ -125,8 +123,6 @@ int main(int argc, char *argv[])
   Directory *dir;
 
   gp_init();
-
-  addrsize = 1;
 
 #define DISPLAY_NOTHING 0
 #define DISPLAY_DIR     1
