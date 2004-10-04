515373d8f80b58964ae97b5df30c8f596f9253f7
nobody <nobody@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2004-10-04 01:49:11 +0000

    This commit was manufactured by cvs2svn to create tag 'cod-3'.

---
+++ tags/cod-3/gputils/gplink/gplink.h
@@ -0,0 +1,115 @@
+/* gplink - GNU PIC Linker
+   Copyright (C) 2001, 2002, 2003, 2004
+   Craig Franklin
+ 
+This file is part of gputils.
+ 
+gputils is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+gputils is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with gputils; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+#ifndef GPLINK_H
+#define GPLINK_H
+
+#define GPLINK_VERSION_STRING ("gplink-" VERSION " alpha")
+
+#define MAX_PATHS 100
+
+struct archivelist {
+  char                *name;
+  gp_archive_type     *archive;
+  struct archivelist  *next;
+};
+
+enum outfile { normal, suppress, named };
+
+extern struct gplink_state {
+  char startdate[80];              /* When gplink ran */
+  enum formats hex_format;         /* format of the output */
+  char *paths[MAX_PATHS];          /* the list of include paths */
+  int numpaths;                    /* number of paths in the list */
+  int byte_addr;                   /* program memory uses byte addressing */
+  enum pic_processor processor;
+  enum proc_class class;
+  enum outfile
+    codfile,			   /* Symbol output file control */
+    hexfile,			   /* Hex output file control */
+    lstfile,			   /* List output file control */
+    mapfile,			   /* Map output file control */
+    objfile;			   /* Executable object file control */
+  int fill_enable;		   /* Fill unused program memory with value */
+  unsigned int fill_value;	   /* Value to fill program memory with */
+  int has_stack;                   /* Has stack directive in linker script */
+  unsigned int stack_size;	   /* Value to fill program memory with */
+  int has_idata;                   /* Has initialized data memory */
+  char  *srcfilename,		   /* Script file name */
+    basefilename[BUFSIZ],	   /* basename for generating hex,list,symbol filenames */
+    codfilename[BUFSIZ],	   /* Symbol (.cod) file name */
+    hexfilename[BUFSIZ],	   /* Hex (.hex) file name */
+    lstfilename[BUFSIZ],	   /* Symbol (.lst) file name */
+    mapfilename[BUFSIZ],	   /* List (.map) file name */
+    objfilename[BUFSIZ];	   /* Object (.o) file name */
+  struct source_context *src;	   /* Top of the stack of the script files */
+  struct {			   /* Map file state: */
+    FILE *f;			     /*   Map file output */
+  } map;
+  struct {			   /* Symbol file state: */
+    FILE *f;			     /*   Symbol file output */
+    int enabled;		     /*   nonzero if symbol file is enabled */
+    int emitting;                    /*   flag indicating when an opcode is emitted */
+  } cod;
+  struct {			   /* List file state: */
+    FILE *f;			     /*   List file output */
+    int enabled;		     /*   nonzero if list file is enabled */
+    struct list_context *src;        /*   list file context */
+    int was_org;                     /*   last address that generated code */
+  } lst;
+  struct {
+    struct symbol_table *definition; /* section definitions from script */
+    struct symbol_table *logical;    /* logical definitions from script */
+  } section;
+  struct {
+    struct symbol_table *definition; /* external symbol definition */
+    struct symbol_table *missing;    /* missing external symbols */
+    struct symbol_table *archive;    /* archive symbol index */
+  } symbol;
+  struct archivelist    *archives;
+  gp_object_type        *object;     /* object files */
+  MemBlock              *i_memory;   /* Instruction memory linked list */
+} state;
+
+struct source_context {
+  char *name;
+  FILE *f;
+  unsigned int line_number;
+  struct yy_buffer_state *yybuf;
+  struct source_context *prev;
+};
+
+struct list_context {
+  char *name;
+  gp_symbol_type *symbol;
+  FILE *f; 
+  gp_boolean missing_source;
+  unsigned int line_number;
+  struct list_context *prev;
+};
+
+void gplink_error(char *messg);
+void gplink_warning(char *messg);
+void gplink_debug(char *messg);
+void gplink_open_coff(char *name);
+void gplink_add_path(char *path);
+
+#endif
---
+++ tags/cod-3/gputils/libgputils/gpsystem.c
@@ -0,0 +1,253 @@
+/* General system functions
+   Copyright (C) 2003, 2004
+   Craig Franklin
+
+This file is part of gputils.
+
+gputils is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+gputils is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gputils; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+#include "stdhdr.h"
+#include "libgputils.h"
+
+char *gp_header_path;
+char *gp_lkr_path;
+char *gp_lib_path;
+char *gp_pub_path;
+
+/* initialize the library */
+
+void
+gp_init(void)
+{
+
+#ifdef USE_DEFAULT_PATHS
+  /* load the environmental variables */
+  gp_header_path = getenv("GPUTILS_HEADER_PATH");
+  gp_lkr_path = getenv("GPUTILS_LKR_PATH");
+  gp_lib_path = getenv("GPUTILS_LIB_PATH");
+  gp_pub_path = getenv("GPUTILS_PUB_PATH");
+
+  #ifndef HAVE_DOS_BASED_FILE_SYSTEM
+    if (gp_header_path == NULL) {
+      gp_header_path = strdup(HEADER_PATH);
+    }
+    if (gp_lkr_path == NULL) {
+      gp_lkr_path = strdup(LKR_PATH);
+    }
+    if (gp_lib_path == NULL) {
+      gp_lib_path = strdup(LIB_PATH);
+    }
+    if (gp_pub_path == NULL) {
+      gp_pub_path = strdup(PUB_PATH);
+    }
+  #endif
+
+#else
+  gp_header_path = NULL;
+  gp_lkr_path = NULL;
+  gp_lib_path = NULL;
+  gp_pub_path = NULL;
+#endif
+
+}
+
+/* little endian functions */
+
+void 
+gp_fputl16(short data, FILE *fp) 
+{
+  fputc((int)(data & 255), fp);
+  fputc((int)((data >> 8) & 255), fp);
+
+  return;
+}
+
+void 
+gp_fputl32(long data, FILE *fp) 
+{
+  fputc((int)(data & 255), fp);
+  fputc((int)((data >> 8) & 255), fp);
+  fputc((int)((data >> 16) & 255), fp);
+  fputc((int)((data >> 24) & 255), fp);
+
+  return;
+}
+
+void 
+gp_fputvar(char *data, int number, FILE *fp) 
+{
+  int i;
+  
+  for(i = 0; i < number; i++)
+    fputc(data[i], fp);
+
+  return;
+}
+
+short 
+gp_getl16(char *addr)
+{
+  short value;
+  
+  value  = (unsigned char)addr[0];
+  value |= (unsigned char)addr[1] << 8;
+  
+  return value;
+}
+
+unsigned short 
+gp_getu16(char *addr)
+{
+  return (unsigned short) gp_getl16(addr);
+}
+
+long 
+gp_getl32(char *addr)
+{
+  long value;
+
+  value  = (unsigned char)addr[0];
+  value |= (unsigned char)addr[1] << 8;
+  value |= (unsigned char)addr[2] << 16;
+  value |= (unsigned char)addr[3] << 24;
+
+  return value;
+}
+
+void 
+gp_putl16(char *addr, short data)
+{
+  addr[1] = (data >> 8) & 0xff;
+  addr[0] = data & 0xff;
+
+  return;
+}
+
+void 
+gp_putl32(char *addr, long data)
+{
+  addr[0] = data & 0xff;
+  addr[1] = (data >> 8)  & 0xff;
+  addr[2] = (data >> 16) & 0xff;
+  addr[3] = (data >> 24) & 0xff;
+
+  return;
+}
+
+/* big endian functions */
+
+long 
+gp_getb32(char *addr)
+{
+  long value;
+
+  value  = (unsigned char)addr[0] << 24;
+  value |= (unsigned char)addr[1] << 16;
+  value |= (unsigned char)addr[2] << 8;
+  value |= (unsigned char)addr[3];
+
+  return value;
+}
+
+void 
+gp_putb32(char *addr, long data)
+{
+  addr[0] = (data >> 24) & 0xff;
+  addr[1] = (data >> 16) & 0xff;
+  addr[2] = (data >> 8)  & 0xff;
+  addr[3] = data & 0xff;
+
+  return;
+}
+
+void
+gp_date_string(char *buffer, size_t sizeof_buffer)
+{
+  time_t now;
+  struct tm *now_tm;
+
+  time(&now);
+  now_tm = localtime(&now);
+  snprintf(buffer, sizeof_buffer,
+           "%d-%d-%d  %02d:%02d:%02d",
+           now_tm->tm_mon + 1,
+           now_tm->tm_mday,
+           1900 + now_tm->tm_year,
+           now_tm->tm_hour,
+           now_tm->tm_min,
+           now_tm->tm_sec);
+
+  return;
+}
+
+char *
+gp_lower_case(char *name)
+{
+  char *new;
+  char *ptr;
+
+  ptr = new = strdup(name);
+
+  while (*ptr != '\0') {
+    *ptr = tolower(*ptr);
+    ptr++;
+  }
+
+  return new;
+}
+
+char *
+gp_upper_case(char *name)
+{
+  char *new;
+  char *ptr;
+
+  ptr = new = strdup(name);
+
+  while (*ptr != '\0') {
+    *ptr = toupper(*ptr);
+    ptr++;
+  }
+
+  return new;
+}
+
+/* linked list functions */
+
+gp_linked_list *
+gp_list_make(void)
+{
+  gp_linked_list *new;
+
+  new = malloc(sizeof(*new));
+  new->annotation = NULL;
+  new->prev = NULL;
+  new->next = NULL;
+
+  return new;
+}
+
+void
+gp_list_annotate(gp_linked_list *link, void *a)
+{
+  link->annotation = a;
+}
+
+void *
+gp_list_get(gp_linked_list *link)
+{
+  return link->annotation;
+}
