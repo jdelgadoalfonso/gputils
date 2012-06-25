5819f4eedd5f99ae14eb0b0e559453d8a3216c55
borutr <borutr@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2012-06-25 06:40:54 +0000

    gputils 0.14.2 release
    git-svn-id: svn://svn.code.sf.net/p/gputils/code@712 2421ac41-8990-4fbb-a74b-d7adc9fc73e3

---
+++ tags/gputils-0_14_2/gputils/gputils/block.c
@@ -0,0 +1,58 @@
+/* Displays contents of ".COD" files
+   Copyright (C) 2001, 2002, 2003, 2004, 2005
+   Scott Dattalo
+   Copyright (C) 2012 Borut Razem
+
+This file is part of gputils.
+
+gputils is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+gputils is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gputils; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+#include "stdhdr.h"
+
+#include "libgputils.h"
+#include "gpvc.h"
+#include "dump.h"
+#include "block.h"
+
+void
+read_block(unsigned char *block, int block_number)
+{
+  fseek(codefile, block_number * COD_BLOCK_SIZE, SEEK_SET);
+  fread(block, COD_BLOCK_SIZE, 1, codefile);
+}
+
+DirBlockInfo *
+read_directory(void)
+{
+  DirBlockInfo *dbi = NULL, *start = NULL;
+  int next_dir_block = 0;
+
+  do {
+    DirBlockInfo *p = malloc(sizeof (DirBlockInfo));
+    assert(p);
+    if (NULL == dbi)
+      start = dbi = p;
+    else {
+      dbi->next = p;
+      dbi = p;
+    }
+    read_block(dbi->dir, next_dir_block);
+  } while (0 != (next_dir_block = gp_getl16(&dbi->dir[COD_DIR_NEXTDIR])));
+
+  dbi->next = NULL;
+
+  return start;
+}
